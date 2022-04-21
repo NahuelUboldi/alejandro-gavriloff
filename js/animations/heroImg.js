0a41870e33c8a1ae879edece5ae5e5e6e404422c
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-04-20 21:56:30 -0300

    hero img effect done

--- js/animations/heroImg.js
+++ js/animations/heroImg.js
@@ -1,20 +1,34 @@
 import { select, selectAll } from '../utils/utilities.js';
 
+const movePaintings = function moveThePaintingsContainer(
+  container,
+  xCalc,
+  yCalc
+) {
+  const ySmoothness = 5;
+  const xSmoothness = 3;
+  return gsap.to(container, {
+    delay: 0.1,
+    duration: 3,
+    rotationY: xCalc / ySmoothness,
+    rotationX: -yCalc / xSmoothness,
+    ease: Power1.inOut,
+  });
+};
+
 const handleMousemove = function handleMouseMovement(e) {
- const { offsetX, offsetY, target } = e;
-  const { clientWidth, clientHeight } = target;
+  const heroPaintingsContainer = select('.contenedor-cuadros');
 
-  const xPos = offsetX / clientWidth - 0.5;
-  const yPos = offsetY / clientHeight - 0.5;
-  console.log(offsetX,clientWidth,offsetY,clientHeight);
+  const xCalc =
+    (e.clientX - heroPaintingsContainer.getBoundingClientRect().left) / 20;
+  const yCalc =
+    (e.clientY - heroPaintingsContainer.getBoundingClientRect().top) / 20;
 
-}
+  movePaintings(heroPaintingsContainer, xCalc, yCalc);
+};
 
+const initHeroAnim = function initializeTheHeroImagesAnimation() {
+  select('#hero').addEventListener('mousemove', handleMousemove);
+};
 
-
-const initHeroAnim = function initializeTheHeroImagesAnimation() { console.log("hola");
- // const heroPaintingsContainer = select(".contenedor-cuadros")
- select("#hero").addEventListener("mousemove",handleMousemove)
-}
-
-export default initHeroAnim
+export default initHeroAnim;
--- js/animations/loader.js
+++ js/animations/loader.js
@@ -3,14 +3,23 @@ const initLoaderAnim = function initializeTheLoaderAnimation() {
   const loader = select('.loader');
   const loaderPaintsContainer = selectAll('.loader-paint');
   const loaderLogo = select('.loader-logo');
-  const pageContent = select(".page-content")
+  const pageContent = select('.page-content');
 
-  const tl = gsap.timeline({defaults:{ duration: 1, ease: Back.easeOut.config(0.7)}});
-  tl
-  .from([loaderPaintsContainer, loaderLogo], {y: '100vh',stagger: 0.05,onUpdate: () => window.scrollTo(0, 0)})
-  .to([loaderPaintsContainer, loaderLogo], {y: '-100vh',stagger: 0.05,ease: Back.easeIn.config(0.7)},"+=1")
-  .to(loader,{yPercent:-100,ease: Power4.easeIn},"-=0.5")
-  .from(pageContent,{y:150,duration:1.2},"-=0.9")
+  const tl = gsap.timeline({
+    defaults: { duration: 1, ease: Back.easeOut.config(0.7) },
+  });
+  tl.from([loaderPaintsContainer, loaderLogo], {
+    y: '100vh',
+    stagger: 0.05,
+    onUpdate: () => window.scrollTo(0, 0),
+  })
+    .to(
+      [loaderPaintsContainer, loaderLogo],
+      { y: '-100vh', stagger: 0.05, ease: Back.easeIn.config(0.7) },
+      '+=1'
+    )
+    .to(loader, { yPercent: -100, ease: Power4.easeIn }, '-=0.5')
+    .from(pageContent, { y: 150, duration: 1.4 }, '-=0.4');
 };
 
 export default initLoaderAnim;
