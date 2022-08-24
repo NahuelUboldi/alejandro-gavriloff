1e2e158522652f7ebea05f10a35e745145051445
NahuelUboldi <nahueluboldi@gmail.com>
2022-08-24 12:25:24 -0300

    keydown events added to modal component functionality

--- js/components/modal.js
+++ js/components/modal.js
@@ -2,41 +2,66 @@ const loadContent = (modalContent, img) => {
   modalContent.innerHTML = `<img src='${img.img.lg}' alt='pintura del artista Alejandro Gavriloff' />`;
 };
 
-const closeModal = (container) => {
-  //!!!!KILL EVENT LISTENERS!!!!!
+const closeModal = (container, handleClick, handleKeydown) => {
   container.classList.remove('show-modal');
+  document.removeEventListener('click', handleClick);
+  document.removeEventListener('keydown', handleKeydown);
+};
+const modifyIndex = (images, imgIndex, operation) => {
+  if (operation === 'sum') {
+    return imgIndex - 1 < 0 ? images.length - 1 : imgIndex - 1;
+  }
+  return imgIndex + 1 >= images.length ? 0 : imgIndex + 1;
 };
 
 const showModal = (images, imgID) => {
   const modalContainer = document.querySelector('.modal-fixed');
-  const modalBg = document.querySelector('.modal-bg');
   const modalContent = document.querySelector('.modal-content');
-  const modalCloseBtn = document.querySelector('#close-modal-btn');
-  const modalArrowLeft = document.querySelector('.fa-chevron-left');
-  const modalArrowRight = document.querySelector('.fa-chevron-right');
 
   modalContainer.classList.add('show-modal');
 
   let imgIndex = images.findIndex((e) => e.id == imgID);
   loadContent(modalContent, images[imgIndex]);
+  const keyHandlers = {
+    ArrowRight: () => {
+      imgIndex = modifyIndex(images, imgIndex, 'sum');
+      loadContent(modalContent, images[imgIndex]);
+    },
+    ArrowLeft: () => {
+      imgIndex = modifyIndex(images, imgIndex, 'subtraction');
+      loadContent(modalContent, images[imgIndex]);
+    },
+    Escape: () => {
+      closeModal(modalContainer, handleClick, handleKeydown);
+    },
+  };
 
-  modalArrowLeft.addEventListener('click', () => {
-    imgIndex = imgIndex - 1 < 0 ? images.length - 1 : imgIndex - 1;
-    loadContent(modalContent, images[imgIndex]);
-  });
+  const clickHandlers = {
+    'modal-right-arrow': () => {
+      imgIndex = modifyIndex(images, imgIndex, 'sum');
+      loadContent(modalContent, images[imgIndex]);
+    },
+    'modal-left-arrow': () => {
+      imgIndex = modifyIndex(images, imgIndex, 'subtraction');
+      loadContent(modalContent, images[imgIndex]);
+    },
+    'modal-close': () => {
+      closeModal(modalContainer, handleClick, handleKeydown);
+    },
+    'modal-bg': () => {
+      closeModal(modalContainer, handleClick, handleKeydown);
+    },
+  };
+  const handleKeydown = (e) => {
+    if (typeof keyHandlers[e.key] == 'function') keyHandlers[e.key]();
+  };
+  const handleClick = (e) => {
+    const element = e.target.dataset.element;
+    if (typeof clickHandlers[element] == 'function') clickHandlers[element]();
+  };
 
-  modalArrowRight.addEventListener('click', () => {
-    imgIndex = imgIndex + 1 >= images.length ? 0 : imgIndex + 1;
-    loadContent(modalContent, images[imgIndex]);
-  });
-
-  modalCloseBtn.addEventListener('click', () => {
-    closeModal(modalContainer);
-  });
-  modalBg.addEventListener('click', (e) => {
-    if ([...e.target.classList].includes('modal-bg'))
-      closeModal(modalContainer);
-  });
+  document.addEventListener('keydown', handleKeydown);
+  document.addEventListener('click', handleClick);
 };
 
 export default showModal;
